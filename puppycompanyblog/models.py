36623732f12a52519c034c7b93da77d3c9a22a09
GentiM <44003210+GentritM@users.noreply.github.com>
2019-07-30 17:02:45 +0200

    Update models.py

--- puppycompanyblog/models.py
+++ puppycompanyblog/models.py
@@ -1,5 +1,5 @@
 #models.py
-from puppycompanyblog import db,login_manager
+from appblog import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
