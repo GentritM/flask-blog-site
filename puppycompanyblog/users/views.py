7cd4f8cbdf9811ece5da52280fb4fb1695d163da
GentiM <44003210+GentritM@users.noreply.github.com>
2019-07-30 17:01:09 +0200

    Update views.py

--- puppycompanyblog/users/views.py
+++ puppycompanyblog/users/views.py
@@ -1,10 +1,10 @@
 # users/views.py
 from flask import render_template,url_for,flash,redirect,request,Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
-from puppycompanyblog import db
-from puppycompanyblog.models import User, BlogPost
-from puppycompanyblog.users.forms import RegistrationForm,LoginForm,UpdateUserForm
-from puppycompanyblog.users.picture_handler import add_profile_pic
+from appblog import db
+from appblog.models import User, BlogPost
+from appblog.users.forms import RegistrationForm,LoginForm,UpdateUserForm
+from appblog.users.picture_handler import add_profile_pic
 
 users = Blueprint('users',__name__)
 
