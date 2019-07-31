1f8d03fda20cc817d508e980e98258049411628a
GentritM <genti_mehmeti@outlook.com>
2019-07-31 10:20:57 +0200

    create post added

--- appblog/__init__.py
+++ appblog/__init__.py
@@ -1,4 +1,3 @@
-# puppycompanyblog/__init__.py
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -10,10 +9,8 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecret'
 
-
-############################
 ### DATABASE SETUP ##########
-########################
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,22 +18,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app,db)
 
-#########################
 # LOGIN CONFIGS
 login_manager = LoginManager()
-
 login_manager.init_app(app)
 login_manager.login_view = 'users.login'
 
 
-
-##################################################
-
-
+#Registering Blueprints
 from appblog.core.views import core
 from appblog.users.views import users
+from appblog.blog_posts.views import blog_posts
 from appblog.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
 app.register_blueprint(error_pages)
